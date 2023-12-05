b4de7c1651eee0276236d1b39a7cd3d44573b2e1
Shubhash Singh <subhashsingh2059@gmail.com>
2023-12-05 14:50:10 +0530

    Grid Challange Solves

--- Day_4_Grid_Challange.java
+++ Day_4_Grid_Challange.java
@@ -1,69 +1,37 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-class Day_4_Grid_Challange {
+public class Day_4_Grid_Challange {
 
-    static boolean isInOrder(String str)
-    {
-        // boolean result = true;
-        // int i=0;
-        // System.out.println("\n\n" + str);
-        for(int i=0;i<str.length()-1;i++)
-        {
-            int a = (int) str.charAt(i);
-            int b = (int)str.charAt(i+1);
-            // int c = (int) str.charAt(str.cha)
-            // System.out.println(a+" "+b);
-            if (a>=b)
-                return false;
-        // System.out.println(result);
-        }
-        return true;
-    }
-    public static boolean isGrid(String[] Grid)
-    {
-        int array_len = Grid.length;
-        int elem_len = Grid[0].length();
-        boolean result = false;
-        for (int i =0;i<elem_len;i++)
-        {
-            String str = "";
-            for (int j=0;j<array_len;j++)
-            {
-                str += Grid[j].charAt(i);
-            }
-            // System.out.println(str);
-            if (isInOrder(str))
-            {
-                result = true;
-            }
-            else{
-                result = false;
-            }
-
-        }
-        return result;
-    }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
-
-        int tc = sc.nextInt();
-        while(tc-->0)
-        {
+        int t = Integer.parseInt(sc.next());
+        while(t-->0){
             int n = sc.nextInt();
-            String[] array = new String[n];
-            for (int i =0;i<n;array[i++]=sc.next());
+            String[]grid = new String[n];
+            for(int i=0;i<n;i++)
+                grid[i] = sort(sc.next());
             
-            // isGrid(array);n
-            if(isGrid(array))
-            {
-                System.out.println("YES");
+            boolean ok = true;
+            int elem_len = grid[0].length();
+            for(int i=0;i<elem_len;i++){
+                for(int j=1;j<n;j++){
+                    if(grid[j].charAt(i) < grid[j-1].charAt(i)){
+                        ok = false;
+                        break;
+                    }
+                }
             }
-            else
-            {
-                System.out.println("NO");
-            }
+            System.out.println(ok?"YES":"NO");
+            
+            
         }
-
         sc.close();
+    }
+    public static String sort(String s){
+        char[] array = s.toCharArray();
+        Arrays.sort(array);
+        return new String(array);
     }
 }
