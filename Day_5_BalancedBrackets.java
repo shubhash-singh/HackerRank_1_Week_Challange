b3fd027cbadd315627b4bba6c471d99ae2087eaf
Shubhash Singh <subhashsingh2059@gmail.com>
2024-03-07 17:40:53 +0530

    Checking the balanced bracked using stack

--- Day_5_BalancedBrackets.java
+++ Day_5_BalancedBrackets.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.Stack;
-import java.util.stream.IntStream;
 
 class CheckBalanceBracket {
 
@@ -31,26 +26,15 @@ class CheckBalanceBracket {
 }
 
 public class Day_5_BalancedBrackets {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the test number : ");
+        int n = Integer.parseInt(sc.nextLine());
 
-        int t = Integer.parseInt(bufferedReader.readLine().trim());
-
-        IntStream.range(0, t).forEach(tItr -> {
-            try {
-                String s = bufferedReader.readLine();
-
-                String result = CheckBalanceBracket.isBalanced(s);
-
-                bufferedWriter.write(result);
-                bufferedWriter.newLine();
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        while(n-->0) {
+            String s = sc.nextLine();
+            System.out.println(CheckBalanceBracket.isBalanced(s));
+        }
+        sc.close();
     }
 }
